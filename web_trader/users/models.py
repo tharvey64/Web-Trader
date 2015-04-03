8dc86d301aa5527a0c3d251fb41dff232052f2b9
Connor Hammond <Connor.hammond@gmail.com>
2015-04-03 18:20:14 -0400

    Fixes some errors

--- web_trader/users/models.py
+++ web_trader/users/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    username = models.CharField(max_length=40)
+    username = models.CharField(max_length=40, unique=True)
     password = models.CharField(max_length=80)
