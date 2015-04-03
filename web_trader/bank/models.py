7428e26a084541785714b62480b1b1b62e56128b
Anthony Keating <anthonyk1225@gmail.com>
2015-04-03 17:11:50 -0400

    added a model to models

--- web_trader/bank/models.py
+++ web_trader/bank/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Client(models.Model):
+	balance = models.CharField(max_length=100)
+	account_number = models.IntegerField()
+	created_at = models.DateField(auto_now=True)
+	updated_at = models.DateField(auto_now_add=True)
