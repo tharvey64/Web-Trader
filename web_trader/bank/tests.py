10240ff5414ff0e68572760cceccc8362e78e020
Connor Hammond <Connor.hammond@gmail.com>
2015-04-05 23:44:46 -0400

    Makes buttons look better for bank page, sets all bank pages to inherit from bank/base.html

--- web_trader/bank/tests.py
+++ web_trader/bank/tests.py
@@ -2,4 +2,4 @@ from django.test import TestCase
 from django.shortcuts import render, redirect
 # Create your tests here.
 def test(request):
-    return render(request, 'bank/welcome.html',{''})
+    return render(request, 'bank/welcome.html')
